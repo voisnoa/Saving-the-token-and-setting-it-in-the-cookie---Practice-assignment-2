085afb707d6d1fd488be3e77cf951bc027fca366
voisnoa <markzlopp@gmail.com>
2025-03-20 11:30:20 +0530

    done the assginment

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+const jwt = require("jsonwebtoken")
+
 const encrypt = (payload, secret) => {
   // your code here and return token
+  const token = jwt.sign(payload,secret, {expiresIn: '60s'});
+  return token
 };
+
+
+const payload = {username : 'john doe'}
+
+const secretkey = "secret";
+
+const token = encrypt(payload , secretkey)
+
+console.log("JWT token" , token)
 
 module.exports = encrypt;
